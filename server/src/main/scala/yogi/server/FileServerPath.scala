463d2b8013c32cd5f5c56c13d0f98c0069c79a7f
Kelton Person <kelton.person@gmail.com>
2016-03-28 22:48:01 -0500

    made static server stream files

--- server/src/main/scala/yogi/server/FileServerPath.scala
+++ server/src/main/scala/yogi/server/FileServerPath.scala
@@ -1,26 +1,26 @@
 package yogi.server
 
-
+import akka.http.scaladsl.model.HttpEntity._
 import akka.http.scaladsl.model._
+import akka.stream.scaladsl.{Source, FileIO}
 
-import java.io.{FileInputStream, File}
-
-import org.apache.commons.io.IOUtils
+import java.io.File
 
 
-case class CachedFile( mediaType: MediaType, bytes: Array[Byte])
+case class CachedFile(mediaType: MediaType, file: File)
 
 // A server path that serves data from the filesystem
-class FileServerPath(rootDirectory: File, index: Option[String] = Some("index.html"), cache: Boolean = false) extends ServerPath {
+class FileServerPath(rootDirectory: File, index: Option[String] = Some("index.html")) extends ServerPath {
 
-  private val fileCache = scala.collection.mutable.Map[String, CachedFile]()
 
   get("*").sync { (req, params) =>
-    val adjustedPath = params("splat").mkString(File.separator)
+    val path = params("splat").head.split("/").mkString(File.separator)
+    val adjustedPath = if(path.startsWith("/")) path.substring(1, path.length) else path
     fetch(adjustedPath) match {
       case Some(data) =>
         val contentType = ContentType(data.mediaType, () => HttpCharsets.`UTF-8`)
-        HttpResponse(entity = HttpEntity(contentType, data.bytes), status = 200)
+        val source:Source[ChunkStreamPart, Any] = FileIO.fromFile(data.file).map(a => Chunk(a)) ++ Source(List(LastChunk))
+        HttpResponse (entity = new Chunked(contentType, source), status = 200)
       case _ => HttpResponse(404)
     }
   }
@@ -34,35 +34,18 @@ class FileServerPath(rootDirectory: File, index: Option[String] = Some("index.ht
         index match {
           case Some(i) =>
             val f = new File(target, i)
-            if(f.exists && f.isFile) Some(read(f, path)) else None
+            val mediaType = MediaTypes.forExtension(suffix(f.getAbsolutePath))
+            if(f.exists && f.isFile) Some(CachedFile(mediaType, f)) else None
           case _ => None
         }
       }
       else {
-        Some(read(target, path))
+        val mediaType = MediaTypes.forExtension(suffix(target.getAbsolutePath))
+        Some(CachedFile(mediaType, target))
       }
     }
     else {
       None
-    }
-  }
-
-  private def read(file: File, targetPath: String, checkCache: Boolean = cache) : CachedFile = {
-    if(!checkCache) {
-      val is = new FileInputStream(file)
-      val bytes = IOUtils.toByteArray(is)
-      val mediaType = MediaTypes.forExtension(suffix(file.getAbsolutePath))
-      val cached = CachedFile(mediaType, bytes)
-      if(cache){
-        fileCache.put(file.getAbsolutePath, cached)
-      }
-      cached
-    }
-    else {
-      fileCache.get(file.getAbsolutePath) match {
-        case Some(a) => a
-        case _ => read(file, targetPath, false)
-      }
     }
   }
 
